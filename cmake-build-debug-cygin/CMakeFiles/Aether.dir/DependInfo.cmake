7471c92f0d837db80eebfe615f28faedebb02a1a
Varun Sethu <varun.sethu1@education.nsw.gov.au>
2018-01-23 20:41:56 +1100

    psuhing errors

---
+++ cmake-build-debug-cygin/CMakeFiles/Aether.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/cygdrive/c/Users/varun/Desktop/Aether/Aether/src/Aether/Common/Errors/Errors.cpp" "/cygdrive/c/Users/varun/Desktop/Aether/cmake-build-debug-cygin/CMakeFiles/Aether.dir/Aether/src/Aether/Common/Errors/Errors.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Aether/libs/boost/boost"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-cygin/CMakeFiles/Aether.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Aether.dir/Aether/src/Aether/Common/Errors/Errors.cpp.o"
+  "../bin/Aether.pdb"
+  "../bin/Aether.exe"
+  "libAether.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Aether.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
